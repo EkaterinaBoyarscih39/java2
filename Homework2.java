2842da65b2a6b62b1948b1196dc78b8df29701c2
Ekaterina Boyarscih <72408556+EkaterinaBoyarscih39@users.noreply.github.com>
2021-06-30 13:55:13 +0500

    Update Homework2.java

--- Homework2.java
+++ Homework2.java
@@ -38,21 +38,21 @@ public class Homework2 {
     // Задача 3. Необходимо написать функцию, которая на вход получает массив строк, в формате имя_игрока количество_очков.
     public static String task_3(String[] entries) {
         HashMap<String, Integer> scores = new HashMap<>();
-        String winner_name="";
-        int winner_score=0;
+        String winner="";
+        int win_score=0;
         for (String entry:entries) {
             String[] e=entry.split(" ");
-            String name=e[0];
+            String player=e[0];
             int score=Integer.parseInt(e[1]);
             int prev_score=scores.getOrDefault(name, 0);
             int cur_score=prev_score+score;
-            scores.put(name, cur_score);
+            scores.put(player, cur_score);
             if (cur_score>winner_score) {
-                winner_score=cur_score;
-                winner_name=name;
+                win_score=cur_score;
+                winner=player;
             }
         }
         System.out.print("Поздравляем с победой, ");
-        return winner_name;
+        return winner;
     }
 }
